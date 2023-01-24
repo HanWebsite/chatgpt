396e7d2b718725e8def8554f5b883b8e63d353c6
Han-DasBoard <123283630+HanWebsite@users.noreply.github.com>
2023-01-24 19:57:04 +0700

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,8 @@
+/**
+ * Instagram: intagram.com/hanggar-GT
+ * github: github.com/HanWebsite
+ */
+
+// get token here https://beta.openai.com/account/api-keys
+API_KEY = ["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"]; // can multiple token
+URL_PROFILE_INSTAGRAM = "https://instagram.com/hanggar_GT";
